a88f01eed646d025fee39a8d1122e60350dbdd34
Dan Cristoloveanu <dcristo@microsoft.com>
2015-03-18 22:02:38 -0700

    Create the amqp lib in the send sample

--- samples/send/main.c
+++ samples/send/main.c
@@ -1,4 +1,14 @@
+#include "amqplib.h"
+
 int main(int argc, char** argv)
 {
+	AMQPLIB_HANDLE amqplib_handle = amqplib_create("localhost", 5671);
+
+	while (1)
+	{
+		(void)amqplib_dowork(amqplib_handle);
+	}
+
+	amqplib_destroy(amqplib_handle);
 	return 0;
 }

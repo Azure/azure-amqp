8279965dbdfa5cd1c1adf567cefd2cbf3ad82564
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-04-17 21:19:44 -0700

    Added a logger.h to define a logging interface

---
+++ amqplib/inc/logger.h
@@ -0,0 +1,14 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+	typedef void(*logger_log)(char* format, ...);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* LOGGER_H */

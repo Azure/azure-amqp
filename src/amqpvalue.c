9412072b5db360c57d9193bc4fdb58ec25196281
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-04-23 00:20:02 -0700

    Add string value to amqpvalue

--- src/amqpvalue.c
+++ src/amqpvalue.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include "amqp_types.h"
 #include "amqpvalue.h"
 
@@ -7,6 +8,8 @@ typedef union AMQP_VALUE_UNION_TAG
 {
 	AMQP_VALUE descriptor;
 	uint64_t ulong;
+	char* string;
+	size_t stringLength;
 } AMQP_VALUE_UNION;
 
 typedef struct AMQP_VALUE_DATA_TAG
@@ -33,6 +36,32 @@ AMQP_VALUE amqpvalue_create_ulong(uint64_t value)
 	{
 		result->type = AMQP_TYPE_ULONG;
 		result->value.ulong = value;
+	}
+	return result;
+}
+
+AMQP_VALUE amqpvalue_create_string(const char* string, uint32_t length)
+{
+	AMQP_VALUE_DATA* result = (AMQP_VALUE_DATA*)malloc(sizeof(AMQP_VALUE_DATA));
+	if (result != NULL)
+	{
+		result->type = AMQP_TYPE_STRING;
+		result->value.stringLength = strlen(string);
+		result->value.string = malloc(result->value.stringLength + 1);
+		if (result->value.string == NULL)
+		{
+			free(result);
+			result = NULL;
+		}
+		else
+		{
+			if (strncpy(result->value.string, string, length) != 0)
+			{
+				free(result->value.string);
+				free(result);
+				result = NULL;
+			}
+		}
 	}
 	return result;
 }

ef71e172ee465935a67cd37b2ce0863182ae78a0
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-05-03 21:30:40 -0700

    Added a unittest project for amqpvalue

--- samples/send/main.c
+++ samples/send/main.c
@@ -13,7 +13,6 @@ int main(int argc, char** argv)
 	{
 		LINK_HANDLE link;
 		AMQP_VALUE payload;
-		MESSAGING_HANDLE messaging;
 		int sent = 0;
 
 		amqplib_handle = amqplib_create("127.0.0.1", 5672);
---
+++ unittests/amqpvalue_unittests/amqpvalue_unittests.cpp
@@ -0,0 +1,15 @@
+#include "TestRunnerSwitcher.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace amqpvalue_unittests
+{		
+	TEST_CLASS(amqpvalue_unittests)
+	{
+	public:
+		
+		TEST_METHOD(x)
+		{
+		}
+	};
+}

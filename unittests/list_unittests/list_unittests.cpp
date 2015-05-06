cb86932eed51e552c48bfa49ff28ea19a35c507f
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-05-05 23:47:40 -0700

    Added list_unittests project

---
+++ unittests/list_unittests/list_unittests.cpp
@@ -0,0 +1,15 @@
+#include "TestRunnerSwitcher.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace amqpvalue_unittests
+{
+	TEST_CLASS(list_unittests)
+	{
+	public:
+
+		TEST_METHOD(x)
+		{
+		}
+	};
+}

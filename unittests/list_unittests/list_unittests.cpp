cb52bdd657532cd2a423349de292e65e473c0372
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-05-10 18:01:56 -0700

    Added test serialization mutex

--- unittests/list_unittests/list_unittests.cpp
+++ unittests/list_unittests/list_unittests.cpp
@@ -2,14 +2,14 @@
 #include "MicroMock.h"
 #include "list.h"
 
-TYPED_MOCK_CLASS(amqp_alloc_mocks, CGlobalMock)
+TYPED_MOCK_CLASS(list_mocks, CGlobalMock)
 {
 public:
 	MOCK_STATIC_METHOD_1(, void*, amqp_malloc, size_t, size)
 	MOCK_METHOD_END(void*, malloc(size));
 };
 
-DECLARE_GLOBAL_MOCK_METHOD_1(amqp_alloc_mocks, , void*, amqp_malloc, size_t, size);
+DECLARE_GLOBAL_MOCK_METHOD_1(list_mocks, , void*, amqp_malloc, size_t, size);
 
 MICROMOCK_MUTEX_HANDLE test_serialize_mutex;
 
@@ -18,9 +18,37 @@ namespace amqpvalue_unittests
 	TEST_CLASS(list_unittests)
 	{
 	public:
+		TEST_CLASS_INITIALIZE(suite_init)
+		{
+			test_serialize_mutex = MicroMockCreateMutex();
+			ASSERT_IS_NOT_NULL(test_serialize_mutex);
+		}
+
+		TEST_CLASS_CLEANUP(suite_cleanup)
+		{
+			MicroMockDestroyMutex(test_serialize_mutex);
+		}
+
+		TEST_METHOD_INITIALIZE(method_init)
+		{
+			if (!MicroMockAcquireMutex(test_serialize_mutex))
+			{
+				ASSERT_FAIL("Could not acquire test serialization mutex.");
+			}
+		}
+
+		TEST_METHOD_CLEANUP(method_cleanup)
+		{
+			if (!MicroMockReleaseMutex(test_serialize_mutex))
+			{
+				ASSERT_FAIL("Could not acquire test serialization mutex.");
+			}
+		}
+
 		TEST_METHOD(when_underlying_calls_suceed_list_create_succeeds)
 		{
 			// arrange
+			list_mocks mocks;
 
 			// act
 			LIST_HANDLE result = list_create();

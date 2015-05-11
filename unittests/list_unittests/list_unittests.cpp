0b6e018daf60864c6ebabec1d6adabffa455efbd
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-05-10 17:58:07 -0700

    Wrote the mocks for amqp_alloc

--- unittests/list_unittests/list_unittests.cpp
+++ unittests/list_unittests/list_unittests.cpp
@@ -1,14 +1,23 @@
 #include "TestRunnerSwitcher.h"
+#include "MicroMock.h"
 #include "list.h"
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+TYPED_MOCK_CLASS(amqp_alloc_mocks, CGlobalMock)
+{
+public:
+	MOCK_STATIC_METHOD_1(, void*, amqp_malloc, size_t, size)
+	MOCK_METHOD_END(void*, malloc(size));
+};
+
+DECLARE_GLOBAL_MOCK_METHOD_1(amqp_alloc_mocks, , void*, amqp_malloc, size_t, size);
+
+MICROMOCK_MUTEX_HANDLE test_serialize_mutex;
 
 namespace amqpvalue_unittests
 {
 	TEST_CLASS(list_unittests)
 	{
 	public:
-
 		TEST_METHOD(when_underlying_calls_suceed_list_create_succeeds)
 		{
 			// arrange

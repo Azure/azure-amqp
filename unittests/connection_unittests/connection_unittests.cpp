6ef7b1a18ba0c4c9b877e7bad89e41d00ef02b5c
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-05-19 21:19:00 -0700

    Added connection unit tests and ignoring some extra files

---
+++ unittests/connection_unittests/connection_unittests.cpp
@@ -0,0 +1,68 @@
+#if 0
+#include "testrunnerswitcher.h"
+#include "micromock.h"
+#include "amqpvalue.h"
+
+#define TEST_IO_HANDLE	(IO_HANDLE)0x4242
+
+TYPED_MOCK_CLASS(connection_mocks, CGlobalMock)
+{
+public:
+	MOCK_STATIC_METHOD_5(, IO_HANDLE, io_create, const IO_INTERFACE_DESCRIPTION*, io_interface_description, void*, io_create_parameters, IO_RECEIVE_CALLBACK, receive_callback, void*, context, LOGGER_LOG, logger_log)
+		MOCK_METHOD_END(IO_HANDLE, TEST_IO_HANDLE);
+	MOCK_STATIC_METHOD_1(, void, amqpalloc_free, void*, ptr)
+		free(ptr);
+	MOCK_VOID_METHOD_END();
+};
+
+extern "C"
+{
+	DECLARE_GLOBAL_MOCK_METHOD_1(amqpvalue_mocks, , void*, amqpalloc_malloc, size_t, size);
+	DECLARE_GLOBAL_MOCK_METHOD_1(amqpvalue_mocks, , void, amqpalloc_free, void*, ptr);
+}
+
+MICROMOCK_MUTEX_HANDLE test_serialize_mutex;
+
+BEGIN_TEST_SUITE(amqpvalue_unittests)
+
+TEST_CLASS_INITIALIZE(suite_init)
+{
+	test_serialize_mutex = MicroMockCreateMutex();
+	ASSERT_IS_NOT_NULL(test_serialize_mutex);
+}
+
+TEST_CLASS_CLEANUP(suite_cleanup)
+{
+	MicroMockDestroyMutex(test_serialize_mutex);
+}
+
+TEST_METHOD_INITIALIZE(method_init)
+{
+	if (!MicroMockAcquireMutex(test_serialize_mutex))
+	{
+		ASSERT_FAIL("Could not acquire test serialization mutex.");
+	}
+	fail_alloc_calls = false;
+}
+
+TEST_METHOD_CLEANUP(method_cleanup)
+{
+	if (!MicroMockReleaseMutex(test_serialize_mutex))
+	{
+		ASSERT_FAIL("Could not release test serialization mutex.");
+	}
+}
+
+/*  */
+
+TEST_METHOD(amqpvalue_create_null_succeeds)
+{
+	// arrange
+
+	// act
+
+	// assert
+}
+
+END_TEST_SUITE(amqpvalue_unittests)
+#endif

38be936b9fdd791022dcdf708b42e0ebb8d62195
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-06-18 22:15:46 -0700

    Moved the generator to its right place

--- amqplib_generator/Program.cs
+++ amqplib_generator/Program.cs
@@ -10,6 +10,9 @@ namespace amqplib_generator
     {
         static void Main(string[] args)
         {
+            amqp_definitions_h amqp_definitions_h = new amqp_definitions_h();
+            string text = amqp_definitions_h.TransformText();
+            System.IO.File.WriteAllText("../../inc/amqp_definitions.h", text);
         }
     }
 }

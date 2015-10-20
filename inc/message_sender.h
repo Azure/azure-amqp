11de5447aa4e09c7e856aab694b383288f716389
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-10-19 22:42:39 -0700

    Add message_sender.c with dummy implementation

--- inc/message_sender.h
+++ inc/message_sender.h
@@ -20,8 +20,6 @@ extern "C" {
 	extern MESSAGE_SENDER_HANDLE messagesender_create(LINK_HANDLE link);
 	extern void messagesender_destroy(MESSAGE_SENDER_HANDLE message_sender);
 	extern int messagesender_send(MESSAGE_SENDER_HANDLE message_sender, MESSAGE_HANDLE message, ON_MESSAGE_SEND_COMPLETE on_message_send_complete, const void* callback_context);
-	extern AMQP_VALUE messaging_create_source(AMQP_VALUE address);
-	extern AMQP_VALUE messaging_create_target(AMQP_VALUE address);
 
 #ifdef __cplusplus
 }
---
+++ src/message_sender.c
@@ -0,0 +1,15 @@
+#include "message_sender.h"
+
+MESSAGE_SENDER_HANDLE messagesender_create(LINK_HANDLE link)
+{
+	return NULL;
+}
+
+void messagesender_destroy(MESSAGE_SENDER_HANDLE message_sender)
+{
+}
+
+int messagesender_send(MESSAGE_SENDER_HANDLE message_sender, MESSAGE_HANDLE message, ON_MESSAGE_SEND_COMPLETE on_message_send_complete, const void* callback_context)
+{
+	return 0;
+}

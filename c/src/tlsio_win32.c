0377f22137693bb3ec025608fb2f2953bcea58b6
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-10-24 17:43:30 -0700

    Removed stack allocation for TLS

--- c/src/tlsio_win32.c
+++ c/src/tlsio_win32.c
@@ -575,52 +575,67 @@ int send_chunk(IO_HANDLE tls_io, const void* buffer, size_t size)
 		}
 		else
 		{
-			SecBuffer security_buffers[4];
-			unsigned char out_buffer[65536];
-			SecBufferDesc security_buffers_desc;
 			SecPkgContext_StreamSizes  sizes;
-
 			SECURITY_STATUS status = QueryContextAttributes(&tls_io_instance->security_context, SECPKG_ATTR_STREAM_SIZES, &sizes);
-
-			memcpy(out_buffer + sizes.cbHeader, buffer, size);
-
-			security_buffers[0].BufferType = SECBUFFER_STREAM_HEADER;
-			security_buffers[0].cbBuffer = sizes.cbHeader;
-			security_buffers[0].pvBuffer = out_buffer;
-			security_buffers[1].BufferType = SECBUFFER_DATA;
-			security_buffers[1].cbBuffer = size;
-			security_buffers[1].pvBuffer = out_buffer + sizes.cbHeader;
-			security_buffers[2].BufferType = SECBUFFER_STREAM_TRAILER;
-			security_buffers[2].cbBuffer = sizes.cbTrailer;
-			security_buffers[2].pvBuffer = out_buffer + sizes.cbHeader + size;
-			security_buffers[3].cbBuffer = 0;
-			security_buffers[3].BufferType = SECBUFFER_EMPTY;
-			security_buffers[3].pvBuffer = 0;
-
-			security_buffers_desc.cBuffers = sizeof(security_buffers) / sizeof(security_buffers[0]);
-			security_buffers_desc.pBuffers = security_buffers;
-			security_buffers_desc.ulVersion = SECBUFFER_VERSION;
-
-			status = EncryptMessage(&tls_io_instance->security_context, 0, &security_buffers_desc, 0);
-			if (FAILED(status))
+			if (status != SEC_E_OK)
 			{
 				result = __LINE__;
 			}
 			else
 			{
-				if (io_send(tls_io_instance->socket_io, out_buffer, security_buffers[0].cbBuffer + security_buffers[1].cbBuffer + security_buffers[2].cbBuffer) != 0)
+				SecBuffer security_buffers[4];
+				SecBufferDesc security_buffers_desc;
+				size_t needed_buffer = sizes.cbHeader + size + sizes.cbTrailer;
+				unsigned char* out_buffer = (unsigned char*)amqpalloc_malloc(needed_buffer);
+				if (out_buffer == NULL)
 				{
 					result = __LINE__;
 				}
 				else
 				{
-					size_t i;
-					for (i = 0; i < size; i++)
+					memcpy(out_buffer + sizes.cbHeader, buffer, size);
+
+					security_buffers[0].BufferType = SECBUFFER_STREAM_HEADER;
+					security_buffers[0].cbBuffer = sizes.cbHeader;
+					security_buffers[0].pvBuffer = out_buffer;
+					security_buffers[1].BufferType = SECBUFFER_DATA;
+					security_buffers[1].cbBuffer = size;
+					security_buffers[1].pvBuffer = out_buffer + sizes.cbHeader;
+					security_buffers[2].BufferType = SECBUFFER_STREAM_TRAILER;
+					security_buffers[2].cbBuffer = sizes.cbTrailer;
+					security_buffers[2].pvBuffer = out_buffer + sizes.cbHeader + size;
+					security_buffers[3].cbBuffer = 0;
+					security_buffers[3].BufferType = SECBUFFER_EMPTY;
+					security_buffers[3].pvBuffer = 0;
+
+					security_buffers_desc.cBuffers = sizeof(security_buffers) / sizeof(security_buffers[0]);
+					security_buffers_desc.pBuffers = security_buffers;
+					security_buffers_desc.ulVersion = SECBUFFER_VERSION;
+
+					status = EncryptMessage(&tls_io_instance->security_context, 0, &security_buffers_desc, 0);
+					if (FAILED(status))
 					{
-						LOG(tls_io_instance->logger_log, 0, "%02x-> ", ((unsigned char*)buffer)[i]);
+						result = __LINE__;
+					}
+					else
+					{
+						if (io_send(tls_io_instance->socket_io, out_buffer, security_buffers[0].cbBuffer + security_buffers[1].cbBuffer + security_buffers[2].cbBuffer) != 0)
+						{
+							result = __LINE__;
+						}
+						else
+						{
+							size_t i;
+							for (i = 0; i < size; i++)
+							{
+								LOG(tls_io_instance->logger_log, 0, "%02x-> ", ((unsigned char*)buffer)[i]);
+							}
+
+							result = 0;
+						}
 					}
 
-					result = 0;
+					amqpalloc_free(out_buffer);
 				}
 			}
 		}

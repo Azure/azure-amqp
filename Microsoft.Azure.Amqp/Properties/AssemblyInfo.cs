04e0d3b1aed58cc53e39827d41a683a5f02e2662
Artur Laksberg <arturl@microsoft.com>
2016-03-28 13:17:09 -0700

    Version bump, NuGet package

--- Microsoft.Azure.Amqp/Properties/AssemblyInfo.cs
+++ Microsoft.Azure.Amqp/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ace7173d-8d6a-4924-a372-884fb10cb711")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 
 // Version information for an assembly follows semantic versioning 1.0.0 (because
 // NuGet didn't support semver 2.0.0 before VS 2015). See semver.org for details.
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]

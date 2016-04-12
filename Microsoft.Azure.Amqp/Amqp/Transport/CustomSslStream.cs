45b2aaa6b78f78031901d75c8e6305b5ae1cda18
David Stucki <dstucki@microsoft.com>
2016-04-12 16:17:16 -0700

    - TransportStream's EndRead(IAsyncResult) doesn't support the full APM model, need to wait for the AsyncCallback before invoking EndRead.
    - As a temporary work-around in DNXCORE50 override SslStream's WriteAsync to avoid the deadlock in SslStream in RC2 timeframe.  This is fixed in newest versions (that I can't get my hands upon).

--- Microsoft.Azure.Amqp/Amqp/Transport/CustomSslStream.cs
+++ Microsoft.Azure.Amqp/Amqp/Transport/CustomSslStream.cs
@@ -5,6 +5,8 @@ namespace Microsoft.Azure.Amqp.Transport
 {
     using System.IO;
     using System.Net.Security;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     sealed class CustomSslStream : SslStream
     {
@@ -38,5 +40,17 @@ namespace Microsoft.Azure.Amqp.Transport
             get;
             internal set;
         }
+
+#if DNXCORE
+        // This is a work-around to a bug in CoreCLR where calling SslStream.ReadAsync (which doesn't complete yet)
+        // followed by calling SslStream.WriteAsync the WriteAsync call blocks until the ReadAsync completes.
+        // Work around this by overriding the base Stream.WriteAsync implementation to avoid this serialization.
+        // Should be removed once we have the fix in SslStream and related classes.
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            base.Write(buffer, offset, count);
+            return TaskHelpers.CompletedTask;
+        }
+#endif
     }
 }

d8262929397df7a55fe67c58b8ad57c7c83966f0
xinchen <xinchen@microsoft.com>
2017-07-28 12:20:39 -0700

    Increase version to 2.1.0

--- Microsoft.Azure.Amqp/Properties/Version.cs
+++ Microsoft.Azure.Amqp/Properties/Version.cs
@@ -3,4 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.5")]
+[assembly: AssemblyInformationalVersion("2.1.0")]

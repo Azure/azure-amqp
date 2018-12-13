6efebe8e2951ea75e8a1fd2d70c35c191278f426
xinchen <xinchen@microsoft.com>
2018-12-12 18:09:58 -0800

    api: AmqpConnectionBase

--- src/AmqpConnectionBase.cs
+++ src/AmqpConnectionBase.cs
@@ -19,6 +19,13 @@ namespace Microsoft.Azure.Amqp
         readonly AsyncIO asyncIO;
         IAmqpUsageMeter usageMeter;
 
+        /// <summary>
+        /// Initializes a connection object.
+        /// </summary>
+        /// <param name="type">A prefix in the object name for debugging related purposes.</param>
+        /// <param name="transport">The transport of the connection.</param>
+        /// <param name="settings"><see cref="AmqpConnectionSettings"/></param>
+        /// <param name="isInitiator">True for transport initiator; false for listener side connections.</param>
         protected AmqpConnectionBase(string type, TransportBase transport, AmqpConnectionSettings settings, bool isInitiator)
             : base(type, transport.Identifier)
         {
@@ -33,26 +40,41 @@ namespace Microsoft.Azure.Amqp
                 this.settings.WriteBufferEmptyLimit, transport, isInitiator);
         }
 
+        /// <summary>
+        /// Gets the connection settings.
+        /// </summary>
         public AmqpConnectionSettings Settings
         {
             get { return this.settings; }
         }
 
+        /// <summary>
+        /// Gets the <see cref="IPrincipal"/> established by the transport.
+        /// </summary>
         public IPrincipal Principal
         {
             get { return this.asyncIO.Transport.Principal; }
         }
 
+        /// <summary>
+        /// Gets the local endpoint.
+        /// </summary>
         public EndPoint LocalEndpoint
         {
             get { return this.asyncIO.Transport.LocalEndPoint; }
         }
 
+        /// <summary>
+        /// Gets the remote endpoint.
+        /// </summary>
         public EndPoint RemoteEndpoint
         {
             get { return this.asyncIO.Transport.RemoteEndPoint; }
         }
 
+        /// <summary>
+        /// Gets or sets a <see cref="IAmqpUsageMeter"/> to monitor incoming and outgoing bytes.
+        /// </summary>
         public IAmqpUsageMeter UsageMeter
         {
             get
@@ -70,11 +92,19 @@ namespace Microsoft.Azure.Amqp
             }
         }
 
+        /// <summary>
+        /// Gets the <see cref="asyncIO"/> of the connection.
+        /// </summary>
         protected AsyncIO AsyncIO
         {
             get { return this.asyncIO; }
         }
 
+        /// <summary>
+        /// Sends a serializable object. The buffer is initialized by the
+        /// <see cref="IAmqpSerializable.Encode(ByteBuffer)"/> method.
+        /// </summary>
+        /// <param name="dataBlock">The serializable object.</param>
         public void SendDatablock(IAmqpSerializable dataBlock)
         {
             ByteBuffer buffer = new ByteBuffer(dataBlock.EncodeSize, true);
@@ -82,20 +112,41 @@ namespace Microsoft.Azure.Amqp
             this.asyncIO.WriteBuffer(buffer);
         }
 
+        /// <summary>
+        /// Sends a buffer.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
         public void SendBuffer(ByteBuffer buffer)
         {
             this.asyncIO.WriteBuffer(buffer);
         }
 
+        /// <summary>
+        /// Sends two buffers. The first may be a command and the second may be optional payload.
+        /// </summary>
+        /// <param name="cmdBuffer">The first buffer.</param>
+        /// <param name="payload">The second buffer.</param>
         public void SendBuffer(ByteBuffer cmdBuffer, ByteBuffer payload)
         {
             this.asyncIO.WriteBuffer(cmdBuffer, payload);
         }
 
+        /// <summary>
+        /// Called when a protocol header is received.
+        /// </summary>
+        /// <param name="header">The protocol header.</param>
         protected abstract void OnProtocolHeader(ProtocolHeader header);
 
+        /// <summary>
+        /// Called when a frame buffer is received.
+        /// </summary>
+        /// <param name="buffer">The frame buffer.</param>
         protected abstract void OnFrameBuffer(ByteBuffer buffer);
 
+        /// <summary>
+        /// The default handler of I/O events from the transport.
+        /// </summary>
+        /// <param name="ioEvent">The transport I/O event.</param>
         protected virtual void HandleIoEvent(IoEvent ioEvent)
         {
         }

7d3177c0ecd8593fe7a2540a6506b1233911f5bb
xinchen <xinchen@microsoft.com>
2020-09-16 09:30:54 -0700

    Increase version to 2.4.7

--- Microsoft.Azure.Amqp/Properties/Version.cs
+++ Microsoft.Azure.Amqp/Properties/Version.cs
@@ -3,4 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyInformationalVersion("2.4.6")]
+[assembly: AssemblyInformationalVersion("2.4.7")]

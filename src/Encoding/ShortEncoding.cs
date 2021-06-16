499c98a0aed4f6f3313186258432cf3b6abd2954
danielmarbach <daniel.marbach@openplace.net>
2021-06-16 19:49:21 +0200

    Short Encoding

--- src/Encoding/ShortEncoding.cs
+++ src/Encoding/ShortEncoding.cs
@@ -3,7 +3,11 @@
 
 namespace Microsoft.Azure.Amqp.Encoding
 {
-    sealed class ShortEncoding : EncodingBase
+    using System;
+    using System.Buffers.Binary;
+    using System.Collections.Generic;
+
+    sealed class ShortEncoding : PrimitiveEncoding<short>
     {
         public ShortEncoding()
             : base(FormatCode.Short)
@@ -58,6 +62,56 @@ namespace Microsoft.Azure.Amqp.Encoding
         public override object DecodeObject(ByteBuffer buffer, FormatCode formatCode)
         {
             return ShortEncoding.Decode(buffer, formatCode);
+        }
+
+        public override int GetArrayEncodeSize(IList<short> value)
+        {
+            return FixedWidth.Short * value.Count;
+        }
+
+        public override void EncodeArray(IList<short> value, ByteBuffer buffer)
+        {
+            int byteCount = FixedWidth.Short * value.Count;
+
+            buffer.Validate(write: true, byteCount);
+
+            Span<byte> destination = buffer.GetWriteSpan();
+
+            if (value is short[] shortArray)
+            {
+                // fast-path for ushort[] so the bounds checks can be elided
+                for (int i = 0; i < shortArray.Length; i++)
+                {
+                    BinaryPrimitives.WriteInt16BigEndian(destination.Slice(FixedWidth.Short * i), shortArray[i]);
+                }
+            }
+            else
+            {
+                IReadOnlyList<short> listValue = (IReadOnlyList<short>)value;
+                for (int i = 0; i < listValue.Count; i++)
+                {
+                    BinaryPrimitives.WriteInt16BigEndian(destination.Slice(FixedWidth.Short * i), listValue[i]);
+                }
+            }
+
+            buffer.Append(byteCount);
+        }
+
+        public override short[] DecodeArray(ByteBuffer buffer, int count, FormatCode formatCode)
+        {
+            int byteCount = FixedWidth.UShort * count;
+            buffer.Validate(write: false, byteCount);
+            ReadOnlySpan<byte> source = buffer.GetReadSpan();
+
+            short[] array = new short[count];
+            for (int i = 0; i < count; ++i)
+            {
+                array[i] = BinaryPrimitives.ReadInt16BigEndian(source.Slice(FixedWidth.Short * i));
+            }
+
+            buffer.Complete(byteCount);
+
+            return array;
         }
     }
 }

f7a0bac8477924913b73019c6400267b7ac9c984
Daniel Marbach <daniel.marbach@openplace.net>
2021-07-15 18:27:47 +0200

    Small improvements around CBS cancellation (#193)
    * Allow delay to be canceled
    
    * Preserve stack trace

--- src/Cbs/AmqpCbsLink.cs
+++ src/Cbs/AmqpCbsLink.cs
@@ -4,6 +4,7 @@
 namespace Microsoft.Azure.Amqp
 {
     using System;
+    using System.Runtime.ExceptionServices;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Amqp.Framing;
@@ -121,7 +122,7 @@ namespace Microsoft.Azure.Amqp
             string address = CbsConstants.CbsAddress;
             AmqpSession session = null;
             RequestResponseAmqpLink link = null;
-            Exception lastException = null;
+            ExceptionDispatchInfo lastException = null;
 
             while (!cancellationToken.IsCancellationRequested)
             {
@@ -146,11 +147,11 @@ namespace Microsoft.Azure.Amqp
                         throw new OperationCanceledException("Connection is closing or closed.", exception);
                     }
 
-                    lastException = exception;
+                    lastException = ExceptionDispatchInfo.Capture(exception);
                     AmqpTrace.Provider.AmqpOpenEntityFailed(this, this.GetType().Name, address, exception);
                 }
 
-                await Task.Delay(1000).ConfigureAwait(false);
+                await Task.Delay(1000, cancellationToken).ConfigureAwait(false);
             }
 
             link?.Abort();
@@ -158,7 +159,8 @@ namespace Microsoft.Azure.Amqp
 
             cancellationToken.ThrowIfCancellationRequested();
 
-            throw lastException;
+            lastException?.Throw();
+            return null;
         }
     }
 }

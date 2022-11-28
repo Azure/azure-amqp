eefab19b634ee99de75e00bf55881deff83ba4eb
Vinay Suryanarayana <vinsu@microsoft.com>
2022-11-27 21:25:24 -0800

    minor nit change

--- src/SendingAmqpLink.cs
+++ src/SendingAmqpLink.cs
@@ -245,7 +245,7 @@ namespace Microsoft.Azure.Amqp
         /// <param name="remoteAttach">The incoming Attach from remote which contains the remote's unsettled delivery states.</param>
         protected override void ProcessUnsettledDeliveries(Attach remoteAttach)
         {
-            if (this.Session.Connection.TerminusStore.TryGetLinkTerminusAsync(this.LinkIdentifier, out AmqpLinkTerminus linkTerminus).GetAwaiter().GetResult())
+            if (this.Session.Connection.TerminusStore.TryGetLinkTerminusAsync(this.LinkIdentifier, out AmqpLinkTerminus linkTerminus).Result)
             {
                 this.deliveriesToBeResentUponRecovery = Task.Run(() => linkTerminus.NegotiateUnsettledDeliveriesAsync(remoteAttach)).Result.Values;
                 this.Opened += ResendDeliveriesOnOpen;

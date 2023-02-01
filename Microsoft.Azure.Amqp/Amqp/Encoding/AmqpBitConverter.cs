4b340b9a41898c2b15389fc60aaa732f6158c944
Xin Chen <xinchen@microsoft.com>
2023-02-01 15:44:37 -0800

    Support big endian system in encoding

--- Microsoft.Azure.Amqp/Amqp/Encoding/AmqpBitConverter.cs
+++ Microsoft.Azure.Amqp/Amqp/Encoding/AmqpBitConverter.cs
@@ -25,49 +25,20 @@ namespace Microsoft.Azure.Amqp.Encoding
 
         public static short ReadShort(ByteBuffer buffer)
         {
-            buffer.Validate(false, FixedWidth.Short);
-            short data;
-            fixed (byte* p = &buffer.Buffer[buffer.Offset])
-            {
-                byte* d = (byte*)&data;
-                d[0] = p[1];
-                d[1] = p[0];
-            }
-
-            buffer.Complete(FixedWidth.Short);
-            return data;
+            return (short)ReadUShort(buffer);
         }
 
         public static ushort ReadUShort(ByteBuffer buffer)
         {
             buffer.Validate(false, FixedWidth.UShort);
-            ushort data;
-            fixed (byte* p = &buffer.Buffer[buffer.Offset])
-            {
-                byte* d = (byte*)&data;
-                d[0] = p[1];
-                d[1] = p[0];
-            }
-
+            ushort data = (ushort)((buffer.Buffer[buffer.Offset] << 8) | buffer.Buffer[buffer.Offset + 1]);
             buffer.Complete(FixedWidth.UShort);
             return data;
         }
 
         public static int ReadInt(ByteBuffer buffer)
         {
-            buffer.Validate(false, FixedWidth.Int);
-            int data;
-            fixed (byte* p = &buffer.Buffer[buffer.Offset])
-            {
-                byte* d = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
-            }
-
-            buffer.Complete(FixedWidth.Int);
-            return data;
+            return (int)ReadUInt(buffer);
         }
 
         public static uint PeekUInt(ByteBuffer buffer)
@@ -87,39 +58,18 @@ namespace Microsoft.Azure.Amqp.Encoding
 
         public static uint ReadUInt(byte[] buffer, int offset, int count)
         {
-            Validate(count, FixedWidth.UInt);
-            uint data;
-            fixed (byte* p = &buffer[offset])
+            uint data = buffer[offset];
+            for (int i = 1; i < FixedWidth.UInt; i++)
             {
-                byte* d = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
+                data <<= 8;
+                data |= buffer[offset + i];
             }
-
             return data;
         }
 
         public static long ReadLong(ByteBuffer buffer)
         {
-            buffer.Validate(false, FixedWidth.Long);
-            long data;
-            fixed (byte* p = &buffer.Buffer[buffer.Offset])
-            {
-                byte* d = (byte*)&data;
-                d[0] = p[7];
-                d[1] = p[6];
-                d[2] = p[5];
-                d[3] = p[4];
-                d[4] = p[3];
-                d[5] = p[2];
-                d[6] = p[1];
-                d[7] = p[0];
-            }
-
-            buffer.Complete(FixedWidth.Long);
-            return data;
+            return (long)ReadULong(buffer);
         }
 
         public static ulong ReadULong(ByteBuffer buffer)
@@ -132,85 +82,43 @@ namespace Microsoft.Azure.Amqp.Encoding
 
         public static ulong ReadULong(byte[] buffer, int offset, int count)
         {
-            Validate(count, FixedWidth.ULong);
-            ulong data;
-            fixed (byte* p = &buffer[offset])
+            ulong data = buffer[offset];
+            for (int i = 1; i < FixedWidth.ULong; i++)
             {
-                byte* d = (byte*)&data;
-                d[0] = p[7];
-                d[1] = p[6];
-                d[2] = p[5];
-                d[3] = p[4];
-                d[4] = p[3];
-                d[5] = p[2];
-                d[6] = p[1];
-                d[7] = p[0];
+                data <<= 8;
+                data |= buffer[offset + i];
             }
-
             return data;
         }
 
         public static float ReadFloat(ByteBuffer buffer)
         {
-            buffer.Validate(false, FixedWidth.Float);
-            float data;
-            fixed (byte* p = &buffer.Buffer[buffer.Offset])
-            {
-                byte* d = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
-            }
-
-            buffer.Complete(FixedWidth.Float);
-            return data;
+            uint data = ReadUInt(buffer);
+            return *((float*)&data);
         }
 
         public static double ReadDouble(ByteBuffer buffer)
         {
-            buffer.Validate(false, FixedWidth.Double);
-            double data;
-            fixed (byte* p = &buffer.Buffer[buffer.Offset])
-            {
-                byte* d = (byte*)&data;
-                d[0] = p[7];
-                d[1] = p[6];
-                d[2] = p[5];
-                d[3] = p[4];
-                d[4] = p[3];
-                d[5] = p[2];
-                d[6] = p[1];
-                d[7] = p[0];
-            }
-
-            buffer.Complete(FixedWidth.Double);
-            return data;
+            ulong data = ReadULong(buffer);
+            return *((double*)&data);
         }
 
         public static Guid ReadUuid(ByteBuffer buffer)
         {
-            buffer.Validate(false, FixedWidth.Uuid);
-            Guid data;
-            fixed (byte* p = &buffer.Buffer[buffer.Offset])
-            {
-                byte* d = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
-
-                d[4] = p[5];
-                d[5] = p[4];
-
-                d[6] = p[7];
-                d[7] = p[6];
-
-                *((ulong*)&d[8]) = *((ulong*)&p[8]);
-            }
-
-            buffer.Complete(FixedWidth.Uuid);
-            return data;
+            uint a = ReadUInt(buffer);
+            ushort b = ReadUShort(buffer);
+            ushort c = ReadUShort(buffer);
+            buffer.Validate(false, FixedWidth.ULong);
+            byte d = buffer.Buffer[buffer.Offset];
+            byte e = buffer.Buffer[buffer.Offset + 1];
+            byte f = buffer.Buffer[buffer.Offset + 2];
+            byte g = buffer.Buffer[buffer.Offset + 3];
+            byte h = buffer.Buffer[buffer.Offset + 4];
+            byte i = buffer.Buffer[buffer.Offset + 5];
+            byte j = buffer.Buffer[buffer.Offset + 6];
+            byte k = buffer.Buffer[buffer.Offset + 7];
+            buffer.Complete(FixedWidth.ULong);
+            return new Guid(a, b, c, d, e, f, g, h, i, j, k);
         }
 
         public static void ReadBytes(ByteBuffer buffer, byte[] data, int offset, int count)
@@ -236,182 +144,94 @@ namespace Microsoft.Azure.Amqp.Encoding
 
         public static void WriteUByte(byte[] buffer, int offset, byte data)
         {
-            Validate(buffer.Length - offset, FixedWidth.UByte);
             buffer[offset] = data;
         }
 
         public static void WriteShort(ByteBuffer buffer, short data)
         {
-            buffer.Validate(true, FixedWidth.Short);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[1];
-                d[1] = p[0];
-            }
-
-            buffer.Append(FixedWidth.Short);
+            WriteUShort(buffer, (ushort)data);
         }
 
         public static void WriteUShort(ByteBuffer buffer, ushort data)
         {
             buffer.Validate(true, FixedWidth.UShort);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[1];
-                d[1] = p[0];
-            }
-
+            WriteUShort(buffer.Buffer, buffer.WritePos, data);
             buffer.Append(FixedWidth.UShort);
         }
 
         public static void WriteUShort(byte[] buffer, int offset, ushort data)
         {
-            Validate(buffer.Length - offset, FixedWidth.UShort);
-            fixed (byte* d = &buffer[offset])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[1];
-                d[1] = p[0];
-            }
+            buffer[offset] = (byte)(data >> 8);
+            buffer[offset + 1] = (byte)data;
         }
 
         public static void WriteInt(ByteBuffer buffer, int data)
         {
-            buffer.Validate(true, FixedWidth.Int);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
-            }
-
-            buffer.Append(FixedWidth.Int);
+            WriteUInt(buffer, (uint)data);
         }
 
         public static void WriteUInt(ByteBuffer buffer, uint data)
         {
             buffer.Validate(true, FixedWidth.UInt);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
-            }
-
+            WriteUInt(buffer.Buffer, buffer.WritePos, data);
             buffer.Append(FixedWidth.UInt);
         }
 
         public static void WriteUInt(byte[] buffer, int offset, uint data)
         {
-            Validate(buffer.Length - offset, FixedWidth.UInt);
-            fixed (byte* d = &buffer[offset])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
-            }
+            buffer[offset] = (byte)(data >> 24);
+            buffer[offset + 1] = (byte)(data >> 16);
+            buffer[offset + 2] = (byte)(data >> 8);
+            buffer[offset + 3] = (byte)data;
         }
 
         public static void WriteLong(ByteBuffer buffer, long data)
         {
-            buffer.Validate(true, FixedWidth.Long);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[7];
-                d[1] = p[6];
-                d[2] = p[5];
-                d[3] = p[4];
-                d[4] = p[3];
-                d[5] = p[2];
-                d[6] = p[1];
-                d[7] = p[0];
-            }
-
-            buffer.Append(FixedWidth.Long);
+            WriteULong(buffer, (ulong)data);
         }
 
         public static void WriteULong(ByteBuffer buffer, ulong data)
         {
             buffer.Validate(true, FixedWidth.ULong);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[7];
-                d[1] = p[6];
-                d[2] = p[5];
-                d[3] = p[4];
-                d[4] = p[3];
-                d[5] = p[2];
-                d[6] = p[1];
-                d[7] = p[0];
-            }
-
+            WriteULong(buffer.Buffer, buffer.WritePos, data);
             buffer.Append(FixedWidth.ULong);
+        }
+
+        internal static void WriteULong(byte[] buffer, int offset, ulong data)
+        {
+            buffer[offset] = (byte)(data >> 56);
+            buffer[offset + 1] = (byte)(data >> 48);
+            buffer[offset + 2] = (byte)(data >> 40);
+            buffer[offset + 3] = (byte)(data >> 32);
+            buffer[offset + 4] = (byte)(data >> 24);
+            buffer[offset + 5] = (byte)(data >> 16);
+            buffer[offset + 6] = (byte)(data >> 8);
+            buffer[offset + 7] = (byte)data;
         }
 
         public static void WriteFloat(ByteBuffer buffer, float data)
         {
-            buffer.Validate(true, FixedWidth.Float);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
-            }
-
-            buffer.Append(FixedWidth.Float);
+            uint n = *((uint*)&data);
+            WriteUInt(buffer, n);
         }
 
         public static void WriteDouble(ByteBuffer buffer, double data)
         {
-            buffer.Validate(true, FixedWidth.Double);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
-            {
-                byte* p = (byte*)&data;
-                d[0] = p[7];
-                d[1] = p[6];
-                d[2] = p[5];
-                d[3] = p[4];
-                d[4] = p[3];
-                d[5] = p[2];
-                d[6] = p[1];
-                d[7] = p[0];
-            }
-
-            buffer.Append(FixedWidth.Double);
+            ulong n = *((ulong*)&data);
+            WriteULong(buffer, n);
         }
 
         public static void WriteUuid(ByteBuffer buffer, Guid data)
         {
             buffer.Validate(true, FixedWidth.Uuid);
-            fixed (byte* d = &buffer.Buffer[buffer.WritePos])
+            byte* p = (byte*)&data;
+            WriteUInt(buffer.Buffer, buffer.WritePos, *((uint*)p));
+            WriteUShort(buffer.Buffer, buffer.WritePos + 4, *((ushort*)(p + 4)));
+            WriteUShort(buffer.Buffer, buffer.WritePos + 6, *((ushort*)(p + 6)));
+            for (int i = 8; i < FixedWidth.Uuid; i++)
             {
-                byte* p = (byte*)&data;
-                d[0] = p[3];
-                d[1] = p[2];
-                d[2] = p[1];
-                d[3] = p[0];
-
-                d[4] = p[5];
-                d[5] = p[4];
-
-                d[6] = p[7];
-                d[7] = p[6];
-
-                *((ulong*)&d[8]) = *((ulong*)&p[8]);
+                buffer.Buffer[buffer.WritePos + i] = *(p + i);
             }
-
             buffer.Append(FixedWidth.Uuid);
         }
 
@@ -420,14 +240,6 @@ namespace Microsoft.Azure.Amqp.Encoding
             buffer.Validate(true, count);
             Buffer.BlockCopy(data, offset, buffer.Buffer, buffer.WritePos, count);
             buffer.Append(count);
-        }
-
-        static void Validate(int bufferSize, int dataSize)
-        {
-            if (bufferSize < dataSize)
-            {
-                throw new AmqpException(AmqpErrorCode.DecodeError, AmqpResources.GetString(AmqpResources.AmqpInsufficientBufferSize, dataSize, bufferSize));
-            }
         }
     }
 }

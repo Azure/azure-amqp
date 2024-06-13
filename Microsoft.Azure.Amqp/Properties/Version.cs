59a1bdeb0d8bb7c77ef2b76cd1389b7106dce75b
Xin Chen <xinchen@microsoft.com>
2024-06-13 14:49:18 -0700

    Increase package version to 2.6.9

--- Microsoft.Azure.Amqp/Properties/Version.cs
+++ Microsoft.Azure.Amqp/Properties/Version.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.8.0")]
-[assembly: AssemblyInformationalVersion("2.6.8")]
+[assembly: AssemblyFileVersion("2.6.9.0")]
+[assembly: AssemblyInformationalVersion("2.6.9")]

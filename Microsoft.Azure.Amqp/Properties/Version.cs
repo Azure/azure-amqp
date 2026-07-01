fdd06c4032fced045f19c30e41e415b29932c44e
Xin Chen (from Dev Box) <xinchen@microsoft.com>
2026-07-01 14:02:55 -0700

    Update package version to 2.7.3

--- Microsoft.Azure.Amqp/Properties/Version.cs
+++ Microsoft.Azure.Amqp/Properties/Version.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.7.0.0")]
-[assembly: AssemblyFileVersion("2.7.2.0")]
-[assembly: AssemblyInformationalVersion("2.7.2")]
+[assembly: AssemblyFileVersion("2.7.3.0")]
+[assembly: AssemblyInformationalVersion("2.7.3")]
